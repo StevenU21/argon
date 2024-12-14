48df580df185b50b7a83c6392374c854c03f2081
StevenU21 <ulloadeifheltsteven@gmail.com>
2024-12-14 14:00:20 -0600

    fix: Update copyright year to be dynamic in the layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -116,8 +116,7 @@
                 <div class="row align-items-center justify-content-xl-between">
                     <div class="col-xl-6">
                         <div class="copyright text-center text-xl-left text-muted">
-                            © 2024 <a href="https://github.com/StevenU21" class="font-weight-bold ml-1"
-                                target="_blank">StevenU21</a>
+                            © {{ date('Y') }} <a href="https://github.com/StevenU21" class="font-weight-bold ml-1" target="_blank">StevenU21</a>
                         </div>
                     </div>
                     <div class="col-xl-6">
