3ac08b0f501b1c4f76234f62444dbf96bb9b1211
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-03-12 01:25:08 -0600

    image component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1103,26 +1103,10 @@
                 <span>Images</span>
             </h2>
             <div class="row">
-                <div class="col-sm-3 col-6">
-                    <small class="d-block text-uppercase font-weight-bold mb-4">Image</small>
-                    <img src="{{ asset('img/theme/team-1-800x800.jpg')}}" alt="Rounded image"
-                        class="img-fluid rounded shadow" style="width: 150px;">
-                </div>
-                <div class="col-sm-3 col-6">
-                    <small class="d-block text-uppercase font-weight-bold mb-4">Circle Image</small>
-                    <img src="{{ asset('img/theme/team-2-800x800.jpg')}}" alt="Circle image"
-                        class="img-fluid rounded-circle shadow" style="width: 150px;">
-                </div>
-                <div class="col-sm-3 col-6 mt-5 mt-sm-0">
-                    <small class="d-block text-uppercase font-weight-bold mb-4">Raised</small>
-                    <img src="{{ asset('img/theme/team-3-800x800.jpg')}}" alt="Raised image"
-                        class="img-fluid rounded shadow-lg" style="width: 150px;">
-                </div>
-                <div class="col-sm-3 col-6 mt-5 mt-sm-0">
-                    <small class="d-block text-uppercase font-weight-bold mb-4">Circle Raised</small>
-                    <img src="{{ asset('img/theme/team-4-800x800.jpg')}}" alt="Raised circle image"
-                        class="img-fluid rounded-circle shadow-lg" style="width: 150px;">
-                </div>
+            <x-welcome.image src="img/theme/team-1-800x800.jpg" alt="Rounded image" class="rounded shadow" style="width: 150px;" label="Image" />
+            <x-welcome.image src="img/theme/team-2-800x800.jpg" alt="Circle image" class="rounded-circle shadow" style="width: 150px;" label="Circle Image" />
+            <x-welcome.image src="img/theme/team-3-800x800.jpg" alt="Raised image" class="rounded shadow-lg" style="width: 150px;" label="Raised" />
+            <x-welcome.image src="img/theme/team-4-800x800.jpg" alt="Raised circle image" class="rounded-circle shadow-lg" style="width: 150px;" label="Circle Raised" />
             </div>
             <h2 class="mt-lg mb-5">
                 <span>Javascript Components</span>
