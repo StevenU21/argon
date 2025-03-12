25c5c5a658646262e8b8c8634aed645e900daf99
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-03-11 19:41:28 -0600

    form label component

--- resources/views/examples/categories/show.blade.php
+++ resources/views/examples/categories/show.blade.php
@@ -10,8 +10,8 @@
                         <h3 class="mb-0"><i class="fas fa-newspaper"></i> Ver Categoria</h3>
                     </div>
                     <div class="col-4 text-right">
-                        <a href="{{ route('categories.index') }}" class="btn btn-sm btn-primary"><i class="fas fa-list"></i>
-                            Volver</a>
+                        <x-link href="{{ route('categories.index') }}" variant="primary" icon="fas fa-arrow-left"
+                            text="Volver" />
                     </div>
                 </div>
             </div>
@@ -20,38 +20,24 @@
                 <div class="pl-lg-4">
                     <div class="row">
                         <div class="col-lg-12">
-                            <div class="form-group">
-                                <label class="form-control-label" for="title"><i class="fas fa-heading"></i>
-                                    Nombre</label>
-                                <p>{{ $category->name }}</p>
-                            </div>
+                            <x-form-label for="name" icon="fas fa-heading" label="Nombre" :value="$category->name" />
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-lg-12">
-                            <div class="form-group">
-                                <label class="form-control-label" for="title"><i class="fas fa-heading"></i>
-                                    Descripción</label>
-                                <p>{{ $category->description }}</p>
-                            </div>
+                            <x-form-label for="description" icon="fas fa-heading" label="Descripción"
+                                :value="$category->description" />
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-lg-12">
-                            <div class="form-group">
-                                <label class="form-control-label" for="content"><i class="fas fa-align-left"></i>
-                                    URL Amigable</label>
-                                <p>{{ $category->slug }}</p>
-                            </div>
+                            <x-form-label for="slug" icon="fas fa-heading" label="Slug" :value="$category->slug" />
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-lg-6">
-                            <div class="form-group">
-                                <label class="form-control-label" for="register_date"><i class="fas fa-calendar-check"></i>
-                                    Fecha de Registro</label>
-                                <p>{{ $category->created_at }}</p>
-                            </div>
+                            <x-form-label for="created_at" icon="fas fa-calendar" label="Creado"
+                                :value="$category->created_at" />
                         </div>
                     </div>
                 </div>
