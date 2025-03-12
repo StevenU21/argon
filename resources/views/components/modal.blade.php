366e434c523c872e83ae3d1b073beee8f04971b7
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-03-11 19:57:03 -0600

    modal component

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,25 @@
+@props([
+    'id' => 'modal-id',
+    'title' => 'Modal Title',
+    'body' => '',
+    'footer' => ''
+])
+
+<div class="modal fade" id="{{ $id }}" tabindex="-1" role="dialog" aria-labelledby="{{ $id }}-label" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title" id="{{ $id }}-label">{{ $title }}</h6>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                {{ $body }}
+            </div>
+            <div class="modal-footer">
+                {{ $footer }}
+            </div>
+        </div>
+    </div>
+</div>
