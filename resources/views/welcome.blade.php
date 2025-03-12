3c39e8c547970fee5fa186e009570ae2e4dd4740
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-03-11 23:00:40 -0600

    form input components

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -272,38 +272,16 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-4 col-sm-6">
-                        <div class="form-group">
-                            <input type="text" placeholder="Regular" class="form-control" />
-                        </div>
-                        <div class="form-group">
-                            <div class="input-group mb-4">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="ni ni-zoom-split-in"></i></span>
-                                </div>
-                                <input class="form-control" placeholder="Search" type="text">
-                            </div>
-                        </div>
+                    <x-welcome.form-input type="text" placeholder="Regular" />
+                    <x-welcome.form-input type="text" placeholder="Search" icon="ni ni-zoom-split-in" />
                     </div>
                     <div class="col-lg-4 col-sm-6">
-                        <div class="form-group">
-                            <input type="text" placeholder="Regular" class="form-control" disabled />
-                        </div>
-                        <div class="form-group">
-                            <div class="input-group mb-4">
-                                <input class="form-control" placeholder="Birthday" type="text">
-                                <div class="input-group-append">
-                                    <span class="input-group-text"><i class="ni ni-zoom-split-in"></i></span>
-                                </div>
-                            </div>
-                        </div>
+                    <x-welcome.form-input type="text" placeholder="Regular" disabled="true" />
+                    <x-welcome.form-input type="text" placeholder="Birthday" icon="ni ni-zoom-split-in" iconPosition="right" />
                     </div>
                     <div class="col-lg-4 col-sm-6">
-                        <div class="form-group has-success">
-                            <input type="text" placeholder="Success" class="form-control is-valid" />
-                        </div>
-                        <div class="form-group has-danger">
-                            <input type="email" placeholder="Error Input" class="form-control is-invalid" />
-                        </div>
+                    <x-welcome.form-input type="text" placeholder="Success" :isValid="true" />
+                    <x-welcome.form-input type="email" placeholder="Error Input" :isValid="false" />
                     </div>
                 </div>
             </div>
