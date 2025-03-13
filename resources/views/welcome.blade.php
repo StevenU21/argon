2b71512242b4e67ad87dac04bca1d9250f4fbddf
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-03-12 18:16:22 -0600

    datepicker and date picker range component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1190,40 +1190,11 @@
             <div class="row">
                 <div class="col-md-4">
                     <small class="d-block text-uppercase font-weight-bold mb-3">Single date</small>
-                    <div class="form-group">
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="ni ni-calendar-grid-58"></i></span>
-                            </div>
-                            <input class="form-control datepicker" placeholder="Select date" type="text"
-                                value="06/20/2018">
-                        </div>
-                    </div>
+                    <x-welcome.datepicker placeholder="Select date" value="06/20/2018" />
                 </div>
                 <div class="col-md-8 mt-4 mt-md-0">
                     <small class="d-block text-uppercase font-weight-bold mb-3">Date range</small>
-                    <div class="input-daterange datepicker row align-items-center">
-                        <div class="col">
-                            <div class="form-group">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-calendar-grid-58"></i></span>
-                                    </div>
-                                    <input class="form-control" placeholder="Start date" type="text" value="06/18/2018">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="form-group">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-calendar-grid-58"></i></span>
-                                    </div>
-                                    <input class="form-control" placeholder="End date" type="text" value="06/22/2018">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <x-welcome.datepicker-range startPlaceholder="Start date" endPlaceholder="End date" startValue="06/18/2018" endValue="06/22/2018" />
                 </div>
             </div>
             <!-- Tooltips and Popovers -->
